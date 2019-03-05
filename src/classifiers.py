45e96dbe0cc7f1e596205e3ba89e53b4a0af51dc
NoName115 <robert.kolcun@gmail.com>
2019-03-05 19:39:11 +0100

    add classifier manager

--- src/classifiers.py
+++ src/classifiers.py
@@ -1,121 +1,125 @@
-from datetime import datetime
+from utils import get_datetime_now_str, get_clf_name
 import subprocess
 import copy
+import json
 import os
 
 
-class WekaManager():
+class ClassifierManager():
 
     # Weka classifiers
     # http://weka.sourceforge.net/doc.dev/weka/classifiers/Classifier.html
 
     def __init__(self, log_folder, weka_jar_path):
         self.log_folder = log_folder
-        if not os.path.isdir(log_folder):
-            os.mkdir(log_folder)
         self.weka_jar_path = weka_jar_path
         if not os.path.exists(self.weka_jar_path):
             raise IOError("Input weka.jar file, \'{0}\' does not exist.".
                 format(self.weka_jar_path))
 
-    def run_weka_classifier(self, clf_name, clf_args, dataset_path):
-        # Check dataset path & classifier name
-        if not os.path.exists(dataset_path):
-            raise IOError("Input dataset \'{0}\' does not exist.".
-                format(dataset_path))
+    @staticmethod
+    def _save_stds(log_folder, output, errors):
+        with open(os.path.join(log_folder, 'stdout.txt'), 'w') as f:
+            f.write(output.decode('UTF-8'))
+        with open(os.path.join(log_folder, 'stderr.txt'), 'w') as f:
+            f.write(errors.decode('UTF-8'))
 
-        # Prepare output file
-        datetime_now_str = datetime.strftime(datetime.now(), "%Y-%m-%d_%H-%M-%S")
-        output_file = os.path.join(self.log_folder,
-                                   clf_name.split('.')[-1] + "_" + datetime_now_str + '.csv')
+    @staticmethod
+    def _save_model_config(log_folder, clf_class, clf_args):
+        with open(os.path.join(log_folder, 'config.json'), 'w') as f:
+            f.write(
+                json.dumps(
+                    {
+                        "class_name": clf_class,
+                        "args": clf_args
+                    },
+                    indent=4,
+                    separators=(',', ':')
+                ))
 
-        # Check clf_args
-        if '-t' in clf_args or '-x' in clf_args or '-classifications' in clf_args:
-            print("Settings \'-t\', \'-x\', \'-classifications\' will be overwritten.")
-
-        # Prepare arguments for classifier
-        run_args = copy.copy(clf_args)
-        run_args['-t'] = dataset_path   # training dataset
-        run_args['-x'] = '5'            # cross-validation folds
-        run_args['-classifications'] = 'weka.classifiers.evaluation.output.prediction.CSV -p 1-last -file {0} -suppress'.\
-            format(output_file)         # output in CSV format
-
+    @staticmethod
+    def _print_run_info(clf_class, run_args):
         # Run classifier
         print("-" * 40)
-        print("Running \'{0}\' with:".format(clf_name))
-        print('\n'.join('\t{0:18} {1}'.format(key, value) for key, value in run_args.items()))
+        print("Running \'{0}\' with:".format(clf_class))
+        for arg in run_args:
+            arg_msg = ' ' + arg + '\n' if arg[0] != '-' else '\t' + arg
+            print(arg_msg, end='')
 
-        # Convert args to args_list
-        args_list = ['java', '-Xmx1024m', '-cp', self.weka_jar_path, clf_name] 
-        for key, value in run_args.items():
-            args_list += [key, value]
-
-        # Run weka
-        p = subprocess.Popen(args_list,
+    @staticmethod
+    def run_subprocess(run_args):
+        p = subprocess.Popen(run_args,
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE)
         output, err = p.communicate()
         rc = p.returncode
+        return output, err, rc
 
+    def prepare_log_folders(self, clf_class, dataset_path):
+        # Create folder names
+        dataset_name, _ = os.path.splitext(os.path.basename(dataset_path))
+        clf_name = get_clf_name(clf_class)
+        run_folder_name = get_datetime_now_str() + "_run"
+        clf_folder_name = get_datetime_now_str() + "_" + clf_name + "_" + dataset_name
+
+        # Create folders
+        folder = os.path.join(self.log_folder,
+                              run_folder_name,
+                              clf_folder_name)
+        os.makedirs(folder, exist_ok=True)
+        return folder, clf_name
+
+    def run_weka_classifier(self, clf_class, clf_args, dataset_path):
+        # Check dataset path
+        if not os.path.exists(dataset_path):
+            raise IOError("Input dataset \'{0}\' does not exist.".
+                format(dataset_path))
+
+        # Prepare output folders
+        log_folder, clf_name = self.prepare_log_folders(clf_class, dataset_path)
+
+        # Check clf_args
+        if '-t' in clf_args or '-x' in clf_args:
+            print("Settings \'-t\', \'-x\' will be overwritten.")
+
+        # Prepare arguments for classifier
+        run_args = copy.copy(clf_args)
+        run_args = [str(arg) for arg in run_args]
+        run_args += ['-t', dataset_path]
+        run_args += ['-x', "5"]
+        #run_args += ['-d', os.path.join(log_folder, clf_name + '.model')]
+        run_args += [
+            '-classifications',
+            'weka.classifiers.evaluation.output.prediction.CSV -p 1-last -file {0} -suppress'.\
+                format(os.path.join(log_folder, 'predict.csv'))
+        ]
+
+        # Print run info
+        self._print_run_info(clf_class, run_args)
+
+        # Add some run args & run classifier
+        run_args = ['java', '-Xmx1024m', '-cp', self.weka_jar_path, clf_class] + run_args
+        output, err, rc = self.run_subprocess(run_args)
+        
         if rc != 0:
             print("[ERROR] Something went wrong!")
 
+        # Save weka outputs, errors and model configuration
+        self._save_stds(log_folder, output, err)
+        self._save_model_config(log_folder, clf_class, clf_args)
 
-def load_config(config_path):
-    import json
-    with open(config_path, 'r') as conf_f:
-        configuration = json.load(conf_f)
+        # Predict labels
+        #self.predict_labels(clf_class, clf_args, dataset_path)
 
-    try:
-        weka_jar_path = configuration['weka_jar_path']
-        classifiers = configuration['classifiers']
-        datasets = configuration['datasets']
-    except KeyError as err:
-        raise KeyError("Parameter {0} is missing in config file.".format(err))
-
-    return weka_jar_path, classifiers, datasets
-
-def _yield_dataset_path(datasets):
-    if 'root_folder' not in datasets:
-        raise KeyError("Parameter \'{0}\' is missing in config file.".format('root_folder'))
-    if 'names' not in datasets:
-        raise KeyError("Parameter \'{0}\' is missing in config file.".format('names'))
-
-    for file_name in datasets['names']:
-        yield os.path.join(datasets['root_folder'], file_name)
-
-def _yield_classificier(classifiers):
-    for clf in classifiers:
-        if 'class_name' not in clf:
-            raise KeyError("Parameter \'{0}\' is missing in config file.".format('class_name'))
-        clf_name = clf['class_name']
-        clf_args = clf['args'] if 'args' in clf else dict()
-        yield clf_name, clf_args
-
-def _valid_config_path(path):
-    import argparse
-    if not os.path.exists(path):
-        raise argparse.ArgumentTypeError("Invalid path to config file.")
-    else:
-        return path
-
-def main_run():
-    import argparse
-    parser = argparse.ArgumentParser(description="PV056-AutoML-testing-framework")
-    parser.add_argument('-c', '--config',
-                        type=_valid_config_path,
-                        help='path to config file',
-                        required=True)
-    args = parser.parse_args()
-
-    weka_jar, classfiers, datasets = load_config(args.config)
-    weka_man = WekaManager('logs/', weka_jar)
-
-    for clf_name, clf_args in _yield_classificier(classfiers):
-        for dataset_path in _yield_dataset_path(datasets):
-            weka_man.run_weka_classifier(clf_name,
-                                         clf_args,
-                                         dataset_path)
-
-if __name__ == "__main__":
-    main_run()
+    def predict_labels(self, clf_class, clf_args, dataset_path):
+        clf_name = get_clf_name(clf_class)
+        run_args = copy.copy(clf_args)
+        run_args += ['-T', dataset_path]
+        run_args += ['-l', os.path.join(self.log_folder, clf_name + '.model')]
+        run_args += [
+            '-classifications',
+            'weka.classifiers.evaluation.output.prediction.CSV -p 1-last -file {0} -suppress'.\
+                format(os.path.join(log_folder, 'predict.csv'))
+        ]
+        run_args = ['java', '-Xmx1024m', '-cp', self.weka_jar_path, clf_class] + run_args
+        _, _, _ = self.run_subprocess(run_args)
---
+++ src/utils.py
@@ -0,0 +1,58 @@
+from data_loader import DataLoader
+from datetime import datetime
+import json
+import os
+
+
+#*********************************************************
+# Utils for classifiers
+#*********************************************************
+
+DATETIME_FORMAT = '%Y-%m-%d_%H-%M-%S'
+
+def load_config_clf(config_path):
+    with open(config_path, 'r') as conf_f:
+        config = json.load(conf_f)
+    try:
+        weka_jar_path = config['weka_jar_path']
+        classifiers = config['classifiers']
+    except KeyError as err:
+        raise KeyError("Parameter {0} is missing in config file.".format(err))
+
+    return weka_jar_path, classifiers
+
+
+def load_config_data(config_path):
+    """
+    with open(config_path, 'r') as conf_f:
+        config = json.load(conf_f)
+    return DataLoader(config).file_paths
+    """
+    return [
+        "weka-3-8-3/data/diabetes.arff",
+        "weka-3-8-3/data/hypothyroid.arff",
+        "weka-3-8-3/data/ionosphere.arff"
+    ]
+
+
+def yield_classifiers(classifiers):
+    for clf in classifiers:
+        if 'class_name' not in clf:
+            raise KeyError("Parameter \'{0}\' is missing in config file.".format('class_name'))
+        clf_name = clf['class_name']
+        clf_args = clf['args'] if 'args' in clf else list()
+        yield clf_name, clf_args
+
+
+def get_datetime_now_str():
+    now = datetime.now()
+    datetime_str = datetime.strftime(now, DATETIME_FORMAT)
+    return datetime_str
+
+
+def get_clf_name(clf_class):
+    return clf_class.split('.')[-1]
+
+#*********************************************************
+# Other utils
+#*********************************************************

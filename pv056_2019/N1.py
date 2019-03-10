81752876a2e79d98988c210f2ea80cdf2d143e05
xbazik00 <445446@mail.muni.cz>
2019-03-10 17:34:55 +0100

    New distance calculation

--- pv056_2019/N1.py
+++ pv056_2019/N1.py
@@ -1,5 +1,7 @@
 from sklearn.metrics import pairwise_distances
 
+# from scipy.spatial import distance
+
 # from scipy.spatial.distance import pdist
 from scipy.sparse.csgraph import minimum_spanning_tree
 import pandas as pd
@@ -8,6 +10,17 @@ import numpy as np
 
 class N1Metric:
     def findFraction(self, df):
+        # num_instances = len(df.values[:, :-1])
+        # print(num_instances)
+
+        # distances = np.zeros((num_instances, num_instances))
+
+        # for i in range(num_instances):
+        #    for j in range(i + 1, num_instances):
+        #        distances[i][j] = distance.euclidean(
+        #            df.values[:, :-1][i], df.values[:, :-1][j]
+        #        )
+
         distances = np.triu(pairwise_distances(df.values[:, :-1]))
         mst = minimum_spanning_tree(distances)
         vertices = [0] * len(df)
@@ -16,6 +29,7 @@ class N1Metric:
             if df.iloc[:, -1][x[0]] != df.iloc[:, -1][x[1]]:
                 vertices[x[0]] = 1
                 vertices[x[1]] = 1
+        print(vertices)
         return sum(vertices) / len(vertices)
 
 

d3895e3a6efc07b8acfbb2cec56d94ba4b491d9e
xbazik00 <445446@mail.muni.cz>
2019-03-11 23:27:38 +0100

    N2 using kNN

--- pv056_2019/N1.py
+++ pv056_2019/N1.py
@@ -1,26 +1,10 @@
 from sklearn.metrics import pairwise_distances
-
-# from scipy.spatial import distance
-
-# from scipy.spatial.distance import pdist
 from scipy.sparse.csgraph import minimum_spanning_tree
-import pandas as pd
 import numpy as np
 
 
 class N1Metric:
     def findFraction(self, df, classes):
-        # num_instances = len(df.values[:, :-1])
-        # print(num_instances)
-
-        # distances = np.zeros((num_instances, num_instances))
-
-        # for i in range(num_instances):
-        #    for j in range(i + 1, num_instances):
-        #        distances[i][j] = distance.euclidean(
-        #            df.values[:, :-1][i], df.values[:, :-1][j]
-        #        )
-
         distances = np.triu(pairwise_distances(df.values))
         mst = minimum_spanning_tree(distances)
         vertices = [0] * len(df)
@@ -32,9 +16,9 @@ class N1Metric:
         return sum(vertices) / len(vertices)
 
 
-if __name__ == "__main__":
-    import sys
-
-    df = pd.read_csv(sys.argv[1], header=None)
-    clf = N1Metric()
-    print(clf.findFraction(df))
+# if __name__ == "__main__":
+#    import sys
+#
+#    df = pd.read_csv(sys.argv[1], header=None)
+#    clf = N1Metric()
+#    print(clf.findFraction(df))
--- pv056_2019/N2.py
+++ pv056_2019/N2.py
@@ -1,34 +1,53 @@
-from sklearn.metrics import pairwise_distances
+from sklearn.neighbors import NearestNeighbors
+
 import numpy as np
 
 
 class N2Metric:
     def findFraction(self, df, classes):
         unique_classes = np.unique(classes)
-        # class_instances = [df.loc[classes == x].values for x in unique_classes]
-        class_instances = [df.loc[classes == x].index for x in unique_classes]
-        distances = pairwise_distances(df.values)
+        num_instances = len(df)
+
+        class_instances = np.array([df.loc[classes == x].index for x in unique_classes])
+
+        self.clf = NearestNeighbors(n_neighbors=num_instances, algorithm="ball_tree")
+        self.clf.fit(df.values)
+
         intra_class = []
         inter_class = []
+
         for cl in range(len(class_instances)):
-            for inst in range(len(class_instances[cl])):
-                # within class
-                intra_class.append(
-                    np.min(
-                        np.take(
-                            distances[class_instances[cl][inst]],
-                            np.delete(class_instances[cl], inst),
-                        )
+            # print(cl)
+            for inst in class_instances[cl]:
+                n_neighbors = min(200, num_instances)
+
+                intra_class_dist = None
+                inter_class_dist = None
+
+                while inter_class_dist is None or intra_class_dist is None:
+                    distances, indices = self.clf.kneighbors(
+                        [df.values[inst]], n_neighbors=n_neighbors
                     )
-                )
-                inter_class.append(
-                    np.min(
-                        np.take(
-                            distances[class_instances[cl][inst]],
-                            np.concatenate(np.delete(class_instances, cl)),
-                        )
-                    )
-                )
+                    for i in range(1, len(indices[0])):
+                        if (
+                            intra_class_dist is None
+                            and indices[0][i] in class_instances[cl]
+                        ):
+                            intra_class_dist = distances[0][i]
+                        if (
+                            inter_class_dist is None
+                            and indices[0][i] not in class_instances[cl]
+                        ):
+                            inter_class_dist = distances[0][i]
+                        if inter_class_dist and intra_class_dist:
+                            break
+                    n_neighbors *= 2
+                    if n_neighbors > num_instances:
+                        n_neighbors = num_instances
+                intra_class.append(intra_class_dist)
+                inter_class.append(inter_class_dist)
+        # print(inter_class)
+        # print(intra_class)
 
         return np.mean(intra_class) / np.mean(inter_class)
 

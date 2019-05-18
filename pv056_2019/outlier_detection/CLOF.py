d3bce89f158d4cbd376d32eceb5e8f64197b73e2
samuelmacko <samuel.macko.sm@gmail.com>
2019-05-19 00:49:47 +0200

    Fixed for more than 2 classes

--- pv056_2019/outlier_detection/CLOF.py
+++ pv056_2019/outlier_detection/CLOF.py
@@ -29,23 +29,32 @@ class CLOFMetric:
 
         for index, row in dataframe.iterrows():
 
-            row_class_index = np.where(unique_classes == row["Class"])[0][0]
-            other_class_index = row_class_index % 1
+            class_column = dataframe.columns.values[-1]
+            row_class_index = np.where(unique_classes == row[class_column])[0][0]
 
             df_counters[row_class_index] += 1
-            class_separated_dataframes[other_class_index].loc[
-                class_separated_dataframes[other_class_index].index[-1] + 1
-                ] = row.values[:-1]
 
-            clf.fit(class_separated_dataframes[other_class_index].values)
-            joined_df_lof = clf._decision_function(class_separated_dataframes[other_class_index].values)
+            other_class_lof_cumulated = 0
+            for i in range(len(class_separated_dataframes)):
 
-            class_separated_dataframes[other_class_index].drop(
-                class_separated_dataframes[other_class_index].index[-1], inplace=True
-            )
+                if i == row_class_index:
+                    continue
+
+                class_separated_dataframes[i].loc[
+                    class_separated_dataframes[i].index[-1] + 1
+                ] = bin_dataframe.loc[index]
+
+                clf.fit(class_separated_dataframes[i].values)
+                joined_df_lof = clf._decision_function(class_separated_dataframes[i].values)
+
+                class_separated_dataframes[i].drop(
+                    class_separated_dataframes[i].index[-1], inplace=True
+                )
+
+                other_class_lof_cumulated += joined_df_lof[-1]
 
             row_class_value = class_separated_lof[row_class_index][df_counters[row_class_index]]
-            other_class_value = joined_df_lof[-1]
+            other_class_value = other_class_lof_cumulated / (len(class_separated_dataframes) - 1)
             df_value = df_lof[index]
 
             values[index] = row_class_value + self.alfa * (1 / other_class_value) + 0.5 * df_value

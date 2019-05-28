b19da0ce5a018a36186511b6f903ea365a32b67a
H00N24 <ondrej.kurak.h@gmail.com>
2019-05-28 14:10:44 +0200

    Adding OD methods to pv056-statistics

--- pv056_2019/statistics.py
+++ pv056_2019/statistics.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 import os
 import re
 import sys
@@ -41,7 +42,28 @@ def main():
     reg = re.compile(r"removed-0*")
     files = sorted([x for x in os.listdir(args["results_dir"]) if x.endswith(".csv")])
 
-    headers = ["Dataset", "Split", "Classifier", "Configuration", "Removed", "Accuracy"]
+    config_file_paths = [
+        x for x in os.listdir(args["results_dir"]) if x.endswith(".json")
+    ]
+
+    config_dict = {}
+    for config_file_path in config_file_paths:
+        with open(os.path.join(args["results_dir"], config_file_path)) as config_file:
+            basename = os.path.basename(config_file_path)
+
+            conf_hash = basename.split("_")[1].replace(".json", "")
+
+            config_dict[conf_hash] = json.load(config_file)
+
+    headers = [
+        "Dataset",
+        "Split",
+        "Classifier",
+        "Outlier detection",
+        "Removed",
+        "Configuration",
+        "Accuracy",
+    ]
     data = []
     for fl in files:
         if not args["pattern"].match(fl):
@@ -55,23 +77,28 @@ def main():
         else:
             file_split.append(0)
 
+        datest, split, classifier, conf_hash, removed = file_split
+
+        classifier = config_dict[conf_hash]["model_config"].get("class_name")
+
+        od_name = config_dict[conf_hash]["ad_config"].get("name", "")
+
         dataframe = pd.read_csv(os.path.join(args["results_dir"], fl))
         all_results = dataframe.shape[0]
         accuracy = np.sum(dataframe["error"] != "+") / all_results
 
-        data.append(file_split + [accuracy])
+        data.append([datest, split, classifier, od_name, removed, conf_hash, accuracy])
 
     if not args["raw"]:
         dataframe = pd.DataFrame(data, columns=headers)
         aggregated_frame = dataframe.groupby(
-            ["Dataset", "Classifier", "Configuration", "Removed"]
+            ["Dataset", "Classifier", "Outlier detection", "Removed", "Configuration"]
         ).mean()
         aggregated_frame = aggregated_frame.loc[:, aggregated_frame.columns != "Split"]
         print(aggregated_frame.to_csv())
     else:
-        print(";".join(headers))
-        for line in data:
-            print(",".join([str(x) for x in line]))
+        dataframe = pd.DataFrame(data, columns=headers)
+        print(dataframe.to_csv(index=False))
 
 
 if __name__ == "__main__":
